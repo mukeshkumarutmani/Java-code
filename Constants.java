15f767cf835fb88168b5eb1002c696d1e7088de9
Mukesh Kumar <kumarmukesh789123@gmail.com>
2023-02-17 18:06:24 +0500

    c-program

---
+++ Constants.java
@@ -0,0 +1,23 @@
+public class Constants
+{
+
+    public static  void main(String[] args)
+    {
+
+
+        System.out.println(" The value of e is " +Math.E);
+        System.out.println(" The value of pi is " +Math.PI);
+        System.out.println(" The value of Square is " +Math.sqrt(16));
+        System.out.println(" The value of power " +Math.pow(2,2));
+        int abs=Math.max(12,4);
+        System.out.println(" The maximum value is  " +abs);
+        System.out.println(" The minimum value is  " +Math.min(12,15));
+        
+        System.out.println(" The value of log " +Math.log(9));
+        double v2=Math.abs(-4.5);
+        System.out.println(" The absolute value of v2 is " +v2);
+        // System.out.println(" The value of log " +Math.log(9));
+
+        
+    }
+}
